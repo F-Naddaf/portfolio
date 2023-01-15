2e234a6e1abcd4f2c0816876291507581dd4232f
Fadi <fady-naddaf@hotmail.com>
2023-01-15 16:40:05 +0100

    Creating Portfolio

---
+++ assets/js/main.js
@@ -0,0 +1,141 @@
+/*--------------Adding class to NavBar to Open and close--------------*/
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
+}
+
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  });
+}
+
+const navLink = document.querySelectorAll('.nav_link');
+function linkAction() {
+  const navMenu = document.getElementById('nav-menu');
+  navMenu.classList.remove('show-menu');
+}
+navLink.forEach((e) => e.addEventListener('click', linkAction));
+
+const skillsContent = document.getElementsByClassName('skills_content');
+const skillsHeader = document.querySelectorAll('.skills_header');
+
+/*--------------Adding class to Skills to Open and close--------------*/
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+  for (i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = 'skills_content skills_close';
+  }
+  if (itemClass === 'skills_content skills_close') {
+    this.parentNode.className = 'skills_content skills_open';
+  }
+}
+
+skillsHeader.forEach((element) => {
+  element.addEventListener('click', toggleSkills);
+});
+
+/*--------------Adding class to Qualification to Open and close--------------*/
+const tabs = document.querySelectorAll('[data-target]');
+const tabContents = document.querySelectorAll('[data-content]');
+
+tabs.forEach((tab) => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target);
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove('qualification_active');
+    });
+    target.classList.add('qualification_active');
+    tabs.forEach((tab) => {
+      tab.classList.remove('qualification_active');
+    });
+    tab.classList.add('qualification_active');
+  });
+});
+
+/*--------------Portfolio Swiper--------------*/
+const swiper = new Swiper('.portfolio_container', {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+/*--------------Scroll to the Top of the Section and Adding color Active button--------------*/
+const sections = document.querySelectorAll('section[id]');
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    const sectionId = current.getAttribute('id');
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector('.nav_menu a[href*=' + sectionId + ']')
+        .classList.add('active-link');
+    } else {
+      document
+        .querySelector('.nav_menu a[href*=' + sectionId + ']')
+        .classList.remove('active-link');
+    }
+  });
+}
+window.addEventListener('scroll', scrollActive);
+
+/*--------------Adding Shadow to Navbar--------------*/
+function scrollHeader() {
+  const nav = document.getElementById('header');
+  if (this.scrollY >= 80) {
+    nav.classList.add('scroll-header');
+  } else {
+    nav.classList.remove('scroll-header');
+  }
+}
+window.addEventListener('scroll', scrollHeader);
+
+/*--------------Show Scroll Arrow--------------*/
+function scrollUp() {
+  const scrollUp = document.getElementById('scroll-up');
+  if (this.scrollY >= 500) {
+    scrollUp.classList.add('show-scroll');
+  } else {
+    scrollUp.classList.remove('show-scroll');
+  }
+}
+window.addEventListener('scroll', scrollUp);
+
+/*--------------Dark Mode--------------*/
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+if(selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click', () => {
+  document.body.classList.toggle(darkTheme)
+  themeButton.classList.toggle(iconTheme)
+
+  localStorage.setItem('selected-theme', getCurrentTheme())
+  localStorage.setItem('selected-icon', getCurrentIcon())
+})
